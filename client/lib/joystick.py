4df8a3d506d20ae5dfab02c1dfea39be6e5aa98a
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2021-05-20 09:37:25 +0200

    Joystick-Library überarbeitet.

--- client/lib/joystick.py
+++ client/lib/joystick.py
@@ -1,68 +1,69 @@
 import machine
 
+CENTER = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+UP = 4
+
+PINS = [32, 39, 34, 36, 33]
+COUNT = len(PINS)
+
+
 class Joystick:
     def __init__(self):
-        self.center_pin = machine.Pin(32, machine.Pin.IN)
-        self.down_pin = machine.Pin(39, machine.Pin.IN)
-        self.left_pin = machine.Pin(34, machine.Pin.IN)
-        self.right_pin = machine.Pin(36, machine.Pin.IN)
-        self.up_pin = machine.Pin(33, machine.Pin.IN)
-        self.center_current = False
-        self.center_last = False
-        self.down_current = False
-        self.down_last = False
-        self.left_current = False
-        self.left_last = False
-        self.right_current = False
-        self.right_last = False
-        self.up_current = False
-        self.up_last = False
+        self.pin = []
+        self.current = [False] * 5
+        self.last = [False] * 5
+        for pin in PINS:
+            self.pin.append(machine.Pin(pin, machine.Pin.IN))
     
+    def pressed(self, button):
+        return self.current[button] and not self.last[button]
+
     def any(self):
-        return self.center_current or self.down_current or self.left_current or self.right_current or self.up_current
+        for i in range(COUNT):
+            if self.current[i]:
+                return True
+        return False
 
     def any_pressed(self):
-        return self.center_pressed() or self.down_pressed() or self.left_pressed() or self.right_pressed() or self.up_pressed()
+        for i in range(COUNT):
+            if self.pressed(i):
+                return True
+        return False
 
     def center(self):
-        return self.center_current
+        return self.current[CENTER]
 
     def center_pressed(self):
-        return self.center_current and not self.center_last
+        return self.pressed(CENTER)
 
     def down(self):
-        return self.down_current
+        return self.current[DOWN]
 
     def down_pressed(self):
-        return self.down_current and not self.down_last
+        return self.pressed(DOWN)
 
     def left(self):
-        return self.left_current
+        return self.current[LEFT]
 
     def left_pressed(self):
-        return self.left_current and not self.left_last
+        return self.pressed(LEFT)
 
     def right(self):
-        return self.right_current
+        return self.current[RIGHT]
 
     def right_pressed(self):
-        return self.right_current and not self.right_last
+        return self.pressed(RIGHT)
 
     def up(self):
-        return self.up_current
+        return self.current[UP]
 
     def up_pressed(self):
-        return self.up_current and not self.up_last
+        return self.pressed(UP)
 
     def update(self):
-        self.center_last = self.center_current
-        self.down_last = self.down_current
-        self.left_last = self.left_current
-        self.right_last = self.right_current
-        self.up_last = self.up_current
-
-        self.center_current = not self.center_pin.value()
-        self.down_current = not self.down_pin.value()
-        self.left_current = not self.left_pin.value()
-        self.right_current = not self.right_pin.value()
-        self.up_current = not self.up_pin.value()
+        for i in range(COUNT):
+            self.last[i] = self.current[i]
+            self.current[i] = not self.pin[i].value()
