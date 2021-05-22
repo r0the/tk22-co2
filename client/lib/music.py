cf0ba7e2112f54f3e7f91aa2acf9636b5713610f
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2021-05-22 10:20:39 +0200

    Renamed variables, removed unecessary code.

--- client/lib/music.py
+++ client/lib/music.py
@@ -4,16 +4,13 @@ import utime
 _buzzer_pin = machine.Pin(26, machine.Pin.OUT)
 _buzzer_pwm = None
 
-melody_index = 7
-melody = "melody/melody" + str(melody_index) + ".txt"
-melody_file = None
+_melody_file = None
 
+melody_index = 7
 melodies_text = ["Axel F", "Drunken Sailor", "Stayin' Alive", "Counting Stars", "Wahn-Sing", "Rasputin", "Coffin Dance", "Pieps"]
 
-jetzt = utime.ticks_ms()
-wecker_utime = jetzt
-
-speed = 60
+_tempo = 60
+_timer = 0
 
 B = 2 ** (1 / 12)
 A = 220
@@ -33,44 +30,39 @@ def buzzer_play(freq):
     _buzzer_pwm.freq(freq)
     _buzzer_pwm.duty(1)
 
-def update_buzzer(element):
-    play_tone(element)
-
 def play_tone(tone):
     if int(tone[2]) == 0:
         buzzer_off()
     else:
-        buzzer_play(tone_to_frequency())
+        buzzer_play(tone_to_frequency(tone[0]))
 
 def update_melody(state, activated, play, last_play, stupid_user, melody_index, melody_changed):
-    global wecker_utime
-    global speed
-    global melody_file
-    jetzt = utime.ticks_ms()
-    melody="melody/melody" + str(melody_index) + ".txt"
-    
+    global _melody_file
+    global _tempo
+    global _timer
+    now = utime.ticks_ms()
+
     if melody_changed:
-        melody_file = None
+        _melody_file = None
     if not last_play and play:
         buzzer_off()
-    if not melody_file:
-        melody_file = open(melody, "r")
-    
+    if not _melody_file:
+        file_name = "melody/melody" + str(melody_index) + ".txt"
+        _melody_file = open(file_name, "r")
     if state != 2 or not play or not activated and not stupid_user:
         buzzer_off()
-    if utime.ticks_diff(wecker_utime, jetzt) <= 0 and play and state == 2 and activated:
-        element = melody_file.readline()
+    if utime.ticks_diff(_timer, now) <= 0 and play and state == 2 and activated:
+        element = _melody_file.readline()
         element = element.replace('\n', '')
         element = element.split(' ')
         if element == ['']:
-            melody_file.close()
-            melody_file = None
+            _melody_file.close()
+            _melody_file = None
         else:
             if len(element) == 3:
-                update_buzzer(element)    
-                wecker = round(float(element[1]) * 1000 * 60) // speed
-                wecker_utime = utime.ticks_add(jetzt, wecker)
+                play_tone(element)    
+                duration = round(float(element[1]) * 1000 * 60) // _tempo
+                _timer = utime.ticks_add(now, duration)
             elif len(element) == 1:
-                speed = int(element[0])
-                wecker = 0
-                wecker_utime = utime.ticks_add(jetzt, wecker)
+                _tempo = int(element[0])
+                _timer = now
